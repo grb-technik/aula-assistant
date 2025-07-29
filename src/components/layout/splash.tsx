d26f71bfb66addcbad3e4b47d4c5c644d1285cd4
Christian <christian0511@gmx.de>
2025-07-29 19:55:20 +0200

    formatting

--- src/components/layout/splash.tsx
+++ src/components/layout/splash.tsx
@@ -1,7 +1,10 @@
 export function SplashScreen() {
     return (
-        <div className="bg-background h-dvh w-dvw grid place-items-center">
-            <img src="/atec_transparent_white.png" className="aspect-square h-1/2 w-auto max-w-1/2 max-h-1/2 object-contain" />
+        <div className="bg-background grid h-dvh w-dvw place-items-center">
+            <img
+                src="/atec_transparent_white.png"
+                className="aspect-square h-1/2 max-h-1/2 w-auto max-w-1/2 object-contain"
+            />
         </div>
     );
 }
